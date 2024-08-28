42bbadbe5e2d798aef2bcf615eade453a1a4afd1
thiagofddias <thiagofdias2@gmail.com>
2024-08-28 17:16:06 -0300

    module 7

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,6 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/courses/entities/courses.entity';
 import { Tag } from 'src/courses/entities/tags.entity';
-import { DataSourceOptions } from 'typeorm';
 
 @Module({
   imports: [
